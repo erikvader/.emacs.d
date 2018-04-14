d2ca2bbd7c9f2ab79e22e6cf51b66cd0aa65cf39
Erik Rimskog <erik.rimskog@gmail.com>
2018-04-14 23:44:42 +0200

    eriks/cleanup-line-dwim

--- myinit.el
+++ myinit.el
@@ -1088,14 +1088,32 @@ side of the sexp"
 (define-key evil-insert-state-map (kbd "C-u") 'sp-up-sexp)
 (define-key evil-insert-state-map (kbd "C-e") 'end-of-line)
 
-(define-key evil-normal-state-map (kbd "C-a") 'eriks/delete-trailing-this-line)
-(define-key evil-insert-state-map (kbd "C-a") 'eriks/delete-trailing-this-line)
+(define-key evil-normal-state-map (kbd "C-a") 'eriks/line-cleanup-dwim)
+(define-key evil-insert-state-map (kbd "C-a") 'eriks/line-cleanup-dwim)
 
 (defun eriks/delete-trailing-this-line ()
   "Removes trailing whitespace from the current line."
   (interactive)
   (let ((l (bounds-of-thing-at-point 'line)))
     (delete-trailing-whitespace (car l) (cdr l))))
+
+(defun eriks/line-cleanup-dwim ()
+  " - if the line is empty, then indent it to the same level as the previous line
+ - if the line is not empty and only contains indentation, then delete the line
+ - if the line is not empty and contains non-indentation characters, then remove trailing whitespace"
+  (interactive)
+  (let* ((l (bounds-of-thing-at-point 'line))
+         (i (current-indentation))
+         (non-i (- (- (cdr l) (car l)) i 1)))
+    (cond
+     ((or (and (> i 0) (= non-i 0)) ; only indentation
+          (> non-i 0))              ; contains non-indentation
+      (eriks/delete-trailing-this-line))
+     ((and (= i 0) (= non-i 0))     ; completely empty line
+      (save-excursion
+        (when (= 0 (forward-line -1))
+          (setq i (current-indentation))))
+      (indent-to i)))))
 
 ;;;;; space motion
 (require 'evil-easymotion)
