51043a462304e07eb8d159f85c93fc963c5885bf
Erik Rimskog <erik.rimskog@gmail.com>
2018-05-14 00:12:04 +0200

    line-cleanup-dwim now also does something on regions

--- myinit.el
+++ myinit.el
@@ -1125,22 +1125,27 @@ side of the sexp"
     (delete-trailing-whitespace (car l) (cdr l))))
 
 (defun eriks/line-cleanup-dwim ()
-  " - if the line is empty, then indent it to the same level as the previous line
+  " - if the mark is active, then remove trailing whitespace inside region
+ - if the line is empty, then indent it to the same level as the previous line
  - if the line is not empty and only contains indentation, then delete the line
  - if the line is not empty and contains non-indentation characters, then remove trailing whitespace"
   (interactive)
-  (let* ((l (bounds-of-thing-at-point 'line))
-         (i (current-indentation))
-         (non-i (- (- (cdr l) (car l)) i 1)))
-    (cond
-     ((or (and (> i 0) (= non-i 0)) ; only indentation
-          (> non-i 0))              ; contains non-indentation
-      (eriks/delete-trailing-this-line))
-     ((and (= i 0) (= non-i 0))     ; completely empty line
-      (save-excursion
-        (when (= 0 (forward-line -1))
-          (setq i (current-indentation))))
-      (indent-to i)))))
+  (if (use-region-p)
+      (progn
+        (delete-trailing-whitespace (region-beginning) (region-end))
+        (deactivate-mark))
+    (let* ((l (bounds-of-thing-at-point 'line))
+           (i (current-indentation))
+           (non-i (- (- (cdr l) (car l)) i 1)))
+      (cond
+       ((or (and (> i 0) (= non-i 0)) ; only indentation
+            (> non-i 0))              ; contains non-indentation
+        (eriks/delete-trailing-this-line))
+       ((and (= i 0) (= non-i 0))     ; completely empty line
+        (save-excursion
+          (when (= 0 (forward-line -1))
+            (setq i (current-indentation))))
+        (indent-to i))))))
 
 ;;;;; space motion
 (require 'evil-easymotion)
