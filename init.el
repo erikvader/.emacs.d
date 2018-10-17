7a7f27191780efbbe9602a4010d2ed17e4d0e958
Erik Rimskog <erik.rimskog@gmail.com>
2018-10-17 22:30:27 +0200

    bunch of latex

--- init.el
+++ init.el
@@ -134,7 +134,7 @@
  '(org-src-window-setup (quote current-window))
  '(package-selected-packages
    (quote
-    (evil-matchit evil-anzu evil-args company matlab-mode what-the-commit highlight-indent-guides vdiff-magit vdiff lua-mode heaven-and-hell arduino-mode ace-window frames-only-mode evil-collection git-timemachine evil-exchange git-gutter-fringe+ yasnippet-snippets wgrep-ag ag drag-stuff which-key smartparens smart-mode-line rainbow-mode rainbow-delimiters projectile outshine org-bullets multiple-cursors markdown-mode magit linum-relative hydra haskell-snippets haskell-mode golden-ratio-scroll-screen ggtags flycheck expand-region evil-nerd-commenter evil-mc-extras evil-lion evil-indent-plus evil-extra-operator disable-mouse diminish counsel-projectile counsel color-moccur charmap buffer-move browse-kill-ring auctex)))
+    (auctex-latexmk evil-matchit evil-anzu evil-args company matlab-mode what-the-commit highlight-indent-guides vdiff-magit vdiff lua-mode heaven-and-hell arduino-mode ace-window frames-only-mode evil-collection git-timemachine evil-exchange git-gutter-fringe+ yasnippet-snippets wgrep-ag ag drag-stuff which-key smartparens smart-mode-line rainbow-mode rainbow-delimiters projectile outshine org-bullets multiple-cursors markdown-mode magit linum-relative hydra haskell-snippets haskell-mode golden-ratio-scroll-screen ggtags flycheck expand-region evil-nerd-commenter evil-mc-extras evil-lion evil-indent-plus evil-extra-operator disable-mouse diminish counsel-projectile counsel color-moccur charmap buffer-move browse-kill-ring auctex)))
  '(perl-continued-brace-offset -3)
  '(perl-continued-statement-offset 3)
  '(perl-indent-level 3)
--- myinit.el
+++ myinit.el
@@ -454,11 +454,45 @@ when set to nil)."
 ;;;; latex
 
 (require 'latex)
+(require 'auctex-latexmk)
+(setq auctex-latexmk-inherit-TeX-PDF-mode t)
+(auctex-latexmk-setup)
+
+;; default viewer
+(setf (cadr (assoc 'output-pdf TeX-view-program-selection)) "Zathura")
+
+(define-key LaTeX-mode-map (kbd "C-c e") 'TeX-error-overview)
+
+(defvar eriks/latex-autocompile nil "Whether the current buffer's after-save-hook should compile the document")
+(make-variable-buffer-local 'eriks/latex-autocompile)
+(defvar eriks/latex-autocompile-command nil "TeX command to auto compile with")
+(make-variable-buffer-local 'eriks/latex-autocompile-command)
+
+(defun eriks/latex-autocompile-toggle ()
+  "Toggles whether the document should be compiled after each save or not.
+Queries the command to use when toggling on."
+  (interactive)
+  (setq eriks/latex-autocompile-command nil)
+  (when (not eriks/latex-autocompile)
+    (setq eriks/latex-autocompile-command (TeX-command-query (TeX-master-file nil nil t))))
+  (setq eriks/latex-autocompile (and (not eriks/latex-autocompile)
+                                     eriks/latex-autocompile-command))
+  (message "auto compilation is %s" (if eriks/latex-autocompile "on" "off")))
+
+(define-key LaTeX-mode-map (kbd "C-c a") 'eriks/latex-autocompile-toggle)
+
+(defun eriks/latex-compile-on-save ()
+  "Auto compile on save if enabled.
+see `eriks/latex-autocompile-toggle'"
+  (when (and eriks/latex-autocompile
+             eriks/latex-autocompile-command)
+    (TeX-command eriks/latex-autocompile-command 'TeX-active-master)))
 
 (defun LaTeX-mode-hook-fun ()
   ;; (modify-syntax-entry ?$ "\"" LaTeX-mode-syntax-table) ;;make $ act like string so smartparens can navigate with it.
-  (define-key LaTeX-mode-map [remap beginning-of-defun] 'LaTeX-find-matching-begin)
-  (define-key LaTeX-mode-map [remap end-of-defun] 'LaTeX-find-matching-end)
+  (add-hook 'after-save-hook 'eriks/latex-compile-on-save nil t)
+  ;; (define-key LaTeX-mode-map [remap beginning-of-defun] 'LaTeX-find-matching-begin)
+  ;; (define-key LaTeX-mode-map [remap end-of-defun] 'LaTeX-find-matching-end)
   (run-hooks 'prog-mode-hook))
 (add-hook 'LaTeX-mode-hook 'LaTeX-mode-hook-fun)
 
