aa3623a369000f0e3ae74afed6af134a9cf6a572
Erik Rimskog <erik.rimskog@gmail.com>
2018-11-04 01:17:43 +0100

    rust-mode

--- init.el
+++ init.el
@@ -140,7 +140,7 @@
  '(org-tags-column 0)
  '(package-selected-packages
    (quote
-    (evil-multiedit evil-org auctex-latexmk evil-args company matlab-mode what-the-commit highlight-indent-guides vdiff-magit vdiff lua-mode heaven-and-hell arduino-mode ace-window frames-only-mode evil-collection git-timemachine evil-exchange git-gutter-fringe+ yasnippet-snippets wgrep-ag ag drag-stuff which-key smartparens smart-mode-line rainbow-mode rainbow-delimiters projectile outshine org-bullets multiple-cursors markdown-mode magit linum-relative hydra haskell-snippets haskell-mode golden-ratio-scroll-screen ggtags flycheck expand-region evil-nerd-commenter evil-mc-extras evil-lion evil-indent-plus evil-extra-operator disable-mouse diminish counsel-projectile counsel color-moccur charmap buffer-move browse-kill-ring auctex)))
+    (rust-mode evil-multiedit evil-org auctex-latexmk evil-args company matlab-mode what-the-commit highlight-indent-guides vdiff-magit vdiff lua-mode heaven-and-hell arduino-mode ace-window frames-only-mode evil-collection git-timemachine evil-exchange git-gutter-fringe+ yasnippet-snippets wgrep-ag ag drag-stuff which-key smartparens smart-mode-line rainbow-mode rainbow-delimiters projectile outshine org-bullets multiple-cursors markdown-mode magit linum-relative hydra haskell-snippets haskell-mode golden-ratio-scroll-screen ggtags flycheck expand-region evil-nerd-commenter evil-mc-extras evil-lion evil-indent-plus evil-extra-operator disable-mouse diminish counsel-projectile counsel color-moccur charmap buffer-move browse-kill-ring auctex)))
  '(perl-continued-brace-offset -3)
  '(perl-continued-statement-offset 3)
  '(perl-indent-level 3)
@@ -155,6 +155,7 @@
      (quote local-map)
      mode-line-minor-mode-keymap)))
  '(rm-whitelist nil)
+ '(rust-indent-offset 3)
  '(select-enable-clipboard nil)
  '(sentence-end-double-space nil)
  '(show-trailing-whitespace nil)
