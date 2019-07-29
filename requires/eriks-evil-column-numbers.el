feb1e03e603615c339dca5757631646fab97f0a1
Erik Rimskog <erik.rimskog@gmail.com>
2019-07-29 20:52:39 +0200

    eriks column numbers fixed from a couple of bugs

--- requires/eriks-evil-column-numbers.el
+++ requires/eriks-evil-column-numbers.el
@@ -1,19 +1,19 @@
-(defun eriks/evil-column-numbers-insert (start flags padding specifier)
+(defun eriks/evil-column-numbers-insert (beg end start flags padding specifier)
   "Inserts increasing numbers in a column. The values start on START
   and are formatted with `format' using the format string
   \"%[FLAGS][PADDING][SPECIFIER]\"."
-  (interactive (progn
-                 (unless (and (evil-visual-state-p)
-                              (eq evil-visual-selection 'block))
-                   (error "no block selection active"))
-                 (list (read-number "Start number: " 1)
-                       (read-string "Flags (default \"\"): " nil nil "")
-                       (or (and current-prefix-arg (prefix-numeric-value))
-                           (read-number "Amount of padding: " 0))
-                       (read-string "Specifier (default \"d\"): " nil nil "d"))))
+  (interactive (list
+                (region-beginning)
+                (region-end)
+                (read-number "Start number: " 1)
+                (read-string "Flags (default \"\"): " nil nil "")
+                (or (and current-prefix-arg
+                         (prefix-numeric-value current-prefix-arg))
+                    (read-number "Amount of padding: " 0))
+                (read-string "Specifier (default \"d\"): " nil nil "d")))
   (rectangle-number-lines
-   (region-beginning)
-   (region-end)
+   beg
+   end
    start
    (concat "%"
            (or flags "")
@@ -24,22 +24,22 @@
              "")
            specifier)))
 
-(defun eriks/evil-column-numbers-insert-numbers (arg)
+(defun eriks/evil-column-numbers-insert-numbers (beg end arg)
   "Inserts numbers in a column starting at 1. Prefix argument is the
 amount of padding to use."
-  (interactive "p")
-  (eriks/evil-column-numbers-insert 1 nil arg "d"))
+  (interactive "r\np")
+  (eriks/evil-column-numbers-insert beg end 1 nil arg "d"))
 
-(defun eriks/evil-column-numbers-insert-numbers-zero (arg)
+(defun eriks/evil-column-numbers-insert-numbers-zero (beg end arg)
   "Inserts zero-padded numbers in a column starting at 1. Prefix
 argument is the amount of padding to use."
-  (interactive "p")
-  (eriks/evil-column-numbers-insert 1 "0" arg "d"))
+  (interactive "r\np")
+  (eriks/evil-column-numbers-insert beg end 1 "0" arg "d"))
 
-(defun eriks/evil-column-numbers-insert-letters (arg)
+(defun eriks/evil-column-numbers-insert-letters (beg end arg)
   "Inserts small letters in a column starting at 'a'. Prefix argument
 is the amount of padding to use."
-  (interactive "p")
-  (eriks/evil-column-numbers-insert 97 nil arg "c"))
+  (interactive "r\np")
+  (eriks/evil-column-numbers-insert beg end 97 nil arg "c"))
 
 (provide 'eriks-evil-column-numbers)
