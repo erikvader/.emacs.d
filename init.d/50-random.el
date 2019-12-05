26c99a3f3eee6b16b8227106729e703dd5c6e077
Erik Rimskog <erik.rimskog@gmail.com>
2019-12-05 23:04:06 +0100

    less restrictive window-deletable-p

--- init.d/50-random.el
+++ init.d/50-random.el
@@ -124,6 +124,39 @@
       (save-buffer))
     (when (kill-buffer)
       (delete-frame)))
+  (defun eriks/window-deletable-p (&optional window)
+    "A copy of `window-deletable-p' where the restriction of
+needing more than one frame is removed. This is to play nice with
+`frames-only-mode' and emacsclient where it is okey to close the
+last frame.
+
+Since 2019-12-05, Emacs version 26.3"
+    (setq window (window-normalize-window window))
+
+    (unless (or ignore-window-parameters
+                (eq (window-parameter window 'delete-window) t))
+      (when (window-parameter window 'window-atom)
+        (setq window (window-atom-root window))))
+
+    (let ((frame (window-frame window)))
+      (cond
+       ((frame-root-window-p window)
+        (unless (or ;; (eq frame (next-frame frame 0)) ;;NOTE: removed this!!
+                    (catch 'other
+                      (dolist (other (frame-list))
+                        (when (and (not (eq other frame))
+                                   (eq (window-frame (minibuffer-window other))
+                                       frame))
+                          (throw 'other t))))
+                    (let ((minibuf (active-minibuffer-window)))
+                      (and minibuf (eq frame (window-frame minibuf)))))
+          'frame))
+       ((window-minibuffer-p window)
+        nil)
+       ((or ignore-window-parameters
+            (not (eq window (window-main-window frame))))
+        t))))
+  (advice-add 'window-deletable-p :override 'eriks/window-deletable-p)
   (frames-only-mode 1)
   :custom
   (frames-only-mode-use-window-functions
