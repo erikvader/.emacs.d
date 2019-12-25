1743e69af76a2c8af5fba42a856e884c80957cdd
Erik Rimskog <erik.rimskog@gmail.com>
2019-12-26 00:37:51 +0100

    lsp toggle_highlight and signature when in insert mode

--- init.d/60-lsp.el
+++ init.d/60-lsp.el
@@ -4,11 +4,35 @@
   (lsp-prefer-flymake nil)
   (lsp-enable-snippet nil)
   (lsp-enable-indentation nil)
-  (lsp-enable-symbol-highlighting nil) ;TODO: change face
+  (lsp-enable-symbol-highlighting nil)
   :gfhook
   ('rust-mode-hook 'lsp) ;; rustup component add rls rust-analysis rust-src
   ('python-mode-hook 'lsp) ;; pacman -S python-language-server python-pyflakes python-rope flake8 python-pycodestyle yapf python-pydocstyle
-  )
+  :config
+  (defun lsp-toggle-highlighting ()
+    "Toggles highlight.
+source: https://github.com/emacs-lsp/lsp-mode/issues/515#issuecomment-564665576"
+    (interactive)
+    (setq lsp-enable-symbol-highlighting (not lsp-enable-symbol-highlighting))
+    (cond
+     ((and lsp-enable-symbol-highlighting  (lsp--capability "documentHighlightProvider"))
+      (add-hook 'lsp-on-idle-hook #'lsp--document-highlight nil t)
+      (lsp--info "Highlighting enabled."))
+     ((not lsp-enable-symbol-highlighting)
+      (remove-hook 'lsp-on-idle-hook #'lsp--document-highlight t)
+      (lsp--remove-overlays 'lsp-highlight)
+      (lsp--info "Highlighting disabled."))
+     (t (user-error "Current server does not support highlights?"))))
+
+  (defun eriks/lsp-hover-or-signature-help ()
+    "Do the hover action thingy when in non-insert and the signature help
+    thingy when in insert.
+source: https://github.com/emacs-lsp/lsp-mode/issues/214#discussion_r242426774"
+    (if (evil-insert-state-p)
+        (lsp-signature)
+      (lsp-hover)))
+  (remove-hook 'lsp-eldoc-hook 'lsp-hover)
+  (add-hook 'lsp-eldoc-hook 'eriks/lsp-hover-or-signature-help))
 
 (use-package lsp-pyls
   :custom
