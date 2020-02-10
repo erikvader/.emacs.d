a917968dc5598f30b2b33a913afe664c466737e2
Erik Rimskog <erik.rimskog@gmail.com>
2020-02-10 23:43:21 +0100

    urxvt -> st

--- requires/eriks-random-stuff.el
+++ requires/eriks-random-stuff.el
@@ -1,6 +1,6 @@
 (defun eriks/spawn-external-terminal ()
   "Spawns a terminal emulator outside of Emacs."
   (interactive)
-  (call-process "setsid" nil 0 nil "urxvt"))
+  (call-process "setsid" nil 0 nil "st"))
 
 (provide 'eriks-random-stuff)
