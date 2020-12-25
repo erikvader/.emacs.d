c97542eb1696562eb00629c810f8a51cec522683
Erik Rimskog <erik.rimskog@gmail.com>
2020-12-25 18:52:23 +0100

    rename current file

--- init.d/50-my-commands.el
+++ init.d/50-my-commands.el
@@ -5,3 +5,16 @@
     (with-output-to-temp-buffer "*describe keymap*"
       (princ (substitute-command-keys (format "\\{%s}" map))))))
 
+(defun eriks/rename-current-file ()
+  "Rename the file the current buffer is currently visiting."
+  (interactive)
+  (let ((current-name (buffer-file-name)))
+    (unless current-name
+      (user-error "current buffer is not visiting a file"))
+    (let ((new-name (read-file-name "rename to " nil nil nil nil nil)))
+      ;;TODO: `expand-file-name'??
+      (when new-name
+        (rename-file current-name new-name nil)
+        (set-visited-file-name new-name nil t)))))
+
+(defalias 'rename-current-file-n-buffer #'eriks/rename-current-file)
