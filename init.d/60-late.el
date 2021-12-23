fd8908c9dfa9a9acdf8785367f781e0c7ef28d02
Erik Rimskog <erik.rimskog@gmail.com>
2021-12-23 12:43:07 +0100

    comment out vdiff

--- init.d/60-late.el
+++ init.d/60-late.el
@@ -1,11 +1,11 @@
-(use-package vdiff-magit
-  :ensure t
-  :config
-  (transient-suffix-put 'magit-dispatch "e" :description "vdiff (dwim)")
-  (transient-suffix-put 'magit-dispatch "e" :command 'vdiff-magit-dwim)
-  (transient-suffix-put 'magit-dispatch "E" :description "vdiff")
-  (transient-suffix-put 'magit-dispatch "E" :command 'vdiff-magit)
-  :general
-  ('magit-mode-map
-   "e" 'vdiff-magit-dwim
-   "E" 'vdiff-magit))
+;; (use-package vdiff-magit
+;;   :ensure t
+;;   :config
+;;   (transient-suffix-put 'magit-dispatch "e" :description "vdiff (dwim)")
+;;   (transient-suffix-put 'magit-dispatch "e" :command 'vdiff-magit-dwim)
+;;   (transient-suffix-put 'magit-dispatch "E" :description "vdiff")
+;;   (transient-suffix-put 'magit-dispatch "E" :command 'vdiff-magit)
+;;   :general
+;;   ('magit-mode-map
+;;    "e" 'vdiff-magit-dwim
+;;    "E" 'vdiff-magit))
