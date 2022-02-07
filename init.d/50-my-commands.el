5018df69abf94c4afb0b7c3ce48b27b667becd75
Erik Rimskog <erik.rimskog@gmail.com>
2022-02-07 20:41:48 +0100

    recompile elpa command

--- init.d/50-my-commands.el
+++ init.d/50-my-commands.el
@@ -22,3 +22,8 @@
 (defun eriks/diction-region (beg end)
   (interactive "r")
   (shell-command-on-region beg end "diction -L en_GB -bs"))
+
+(defun eriks/recompile-elpa ()
+  "Compile every .el-file in 'elpa'."
+  (interactive)
+  (byte-recompile-directory (concat user-emacs-directory "elpa") 0 t))
