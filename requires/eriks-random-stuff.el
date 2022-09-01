eafe5fccd2b892eebbfd54437871e0d69967e0c1
Erik Rimskog <erik.rimskog@gmail.com>
2022-09-01 21:33:27 +0200

    spawn terminal the right command

--- requires/eriks-random-stuff.el
+++ requires/eriks-random-stuff.el
@@ -1,6 +1,6 @@
 (defun eriks/spawn-external-terminal ()
   "Spawns a terminal emulator outside of Emacs."
   (interactive)
-  (call-process "setsid" nil 0 nil "st"))
+  (call-process "setsid" nil 0 nil "st-tmux"))
 
 (provide 'eriks-random-stuff)
