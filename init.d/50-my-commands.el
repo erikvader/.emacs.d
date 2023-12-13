0a952a376920b2e1cea3f47a6c505bf957a7d9d8
Erik Rimskog <erik.rimskog@gmail.com>
2023-12-13 22:39:58 +0100

    eriks/kill-buffers-of-removed-files

--- init.d/50-my-commands.el
+++ init.d/50-my-commands.el
@@ -13,3 +13,14 @@
   "Compile every .el-file in 'elpa'."
   (interactive)
   (byte-recompile-directory (concat user-emacs-directory "elpa") 0 t))
+
+(defun eriks/kill-buffers-of-removed-files ()
+  "Prompts to kill every buffer that is visiting a non-existent file."
+  (interactive)
+  (map-y-or-n-p
+   (lambda (buffer)
+     (if-let ((filename (buffer-file-name buffer))
+              ((not (file-exists-p filename))))
+         (format "Kill buffer '%s' visiting removed '%s'? " (buffer-name buffer) filename)))
+   #'kill-buffer
+   (buffer-list)))
