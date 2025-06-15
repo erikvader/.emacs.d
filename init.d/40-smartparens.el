321db7f0423a4bc7bf0ef81a411eb88c0ae00f7a
Erik Rimskog <erik.rimskog@gmail.com>
2025-06-16 00:36:40 +0200

    smartparens repeat bindings

--- init.d/40-smartparens.el
+++ init.d/40-smartparens.el
@@ -3,12 +3,14 @@
   :diminish
   :config
   (require 'smartparens-config)
+
   (defun eriks/create--newline-and-enter-sexp (&rest _ignored)
     "Open a new brace or bracket expression, with relevant newlines and indentation."
     (newline)
     (indent-according-to-mode)
     (forward-line -1)
     (indent-according-to-mode))
+
   (defun eriks/sp-open-on (paren modes)
     "Makes all delimeter openers in paren \"open\" after enter has been pressed."
     (when (stringp paren)
@@ -16,6 +18,20 @@
     (dolist (i paren)
       (sp-local-pair modes i nil :post-handlers '((eriks/create--newline-and-enter-sexp "RET")
                                                   (eriks/create--newline-and-enter-sexp "<return>")))))
+
+  (eriks/bind-key-repeat sp-slurp-barf-repeat-map
+    :states 'normal
+    :prefix eriks/leader
+    "]" 'sp-forward-slurp-sexp
+    "[" 'sp-backward-slurp-sexp
+    "}" 'sp-forward-barf-sexp
+    "{" 'sp-backward-barf-sexp)
+
+  (eriks/bind-key-repeat sp-transpose-repeat-map
+    :states 'normal
+    :prefix eriks/leader
+    "C-t" 'sp-transpose-sexp)
+
   :ghook ('prog-mode-hook '(show-smartparens-mode smartparens-mode))
   :custom
   (sp-autodelete-closing-pair nil)
@@ -29,12 +45,11 @@
   :general
   ('normal
    :prefix eriks/leader
-   "]" 'sp-forward-slurp-sexp
-   "[" 'sp-backward-slurp-sexp
-   "}" 'sp-forward-barf-sexp
-   "{" 'sp-backward-barf-sexp)
+   "C-s" 'sp-split-sexp
+   "C-j" 'sp-join-sexp
+   "C-d" 'sp-splice-sexp-killing-around)
   ('motion
-   "]" 'sp-forward-sexp ;; sp-forward-parallel-sexp
+   "]" 'sp-forward-sexp
    "[" 'sp-backward-sexp
    "<" 'sp-backward-up-sexp
    ">" 'sp-up-sexp
