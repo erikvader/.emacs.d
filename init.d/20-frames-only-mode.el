9395d217dd93a6c57f17d513142ad452a4bd7ce7
Erik Rimskog <erik.rimskog@gmail.com>
2025-07-02 23:05:20 +0200

    update frames-only-mode

--- init.d/20-frames-only-mode.el
+++ init.d/20-frames-only-mode.el
@@ -3,13 +3,20 @@
 kinds of config to optimize for a single frame are loaded if
 nil.")
 
-(defun eriks/frames-only-use-window-funcs (fun)
+(defun eriks/frames-only-use-window-funcs (&rest funs)
   "add FUN to `frames-only-mode-use-window-functions'"
   (when (featurep 'frames-only-mode)
-    (add-to-list 'frames-only-mode-use-window-functions fun)
-    ;; have to do this manually if mode already is activated
-    (when frames-only-mode
-      (advice-add fun :around #'frames-only-mode-advice-use-windows))))
+    (dolist (fun funs)
+      (add-to-list 'frames-only-mode-use-window-functions fun)
+      ;; have to do this manually if mode already is activated
+      (when frames-only-mode
+        (advice-add fun :around #'frames-only-mode-advice-use-windows)))))
+
+(defun eriks/frames-only-kill-frame-if (&rest bufs)
+  "add FUN to `frames-only-mode-kill-frame-when-buffer-killed-buffer-list'"
+  (when (featurep 'frames-only-mode)
+    (dolist (buf bufs)
+      (add-to-list 'frames-only-mode-kill-frame-when-buffer-killed-buffer-list buf))))
 
 (use-package frames-only-mode
   :if eriks/use-frames-only-mode
@@ -43,14 +50,14 @@ Since 2019-12-05, Emacs version 26.3"
       (cond
        ((frame-root-window-p window)
         (unless (or ;; (eq frame (next-frame frame 0)) ;;NOTE: removed this!!
-                    (catch 'other
-                      (dolist (other (frame-list))
-                        (when (and (not (eq other frame))
-                                   (eq (window-frame (minibuffer-window other))
-                                       frame))
-                          (throw 'other t))))
-                    (let ((minibuf (active-minibuffer-window)))
-                      (and minibuf (eq frame (window-frame minibuf)))))
+                 (catch 'other
+                   (dolist (other (frame-list))
+                     (when (and (not (eq other frame))
+                                (eq (window-frame (minibuffer-window other))
+                                    frame))
+                       (throw 'other t))))
+                 (let ((minibuf (active-minibuffer-window)))
+                   (and minibuf (eq frame (window-frame minibuf)))))
           'frame))
        ((window-minibuffer-p window)
         nil)
@@ -59,10 +66,15 @@ Since 2019-12-05, Emacs version 26.3"
         t))))
   (advice-add 'window-deletable-p :override 'eriks/window-deletable-p)
 
-  (add-to-list 'frames-only-mode-use-window-functions #'Custom-newline) ;; customize actions window
-  (add-to-list 'frames-only-mode-kill-frame-when-buffer-killed-buffer-list "widget-choose") ;; actually close customize's action window
+  (eriks/frames-only-kill-frame-if
+   "widget-choose" ;; actually close customize's action window
+   )
 
-  (add-to-list 'frames-only-mode-use-window-functions #'display-warning) ;; logs from byte compilation for example
+  (eriks/frames-only-use-window-funcs
+   #'read-multiple-choice ;; For the help buffer that can be shown
+   #'display-warning ;; logs from byte compilation for example
+   #'Custom-newline ;; customize actions window
+   )
 
   (frames-only-mode 1)
   :general-config
