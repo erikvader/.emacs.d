a0d8cc9a8bd1ee00c9af055752b7cbd9b42c0410
Erik Rimskog <erik.rimskog@gmail.com>
2025-09-14 21:58:57 +0200

    remove the evil-easymotion submodule

--- init.d/30-avy.el
+++ init.d/30-avy.el
@@ -22,10 +22,18 @@
   (eriks/leader-def 'motion
     "SPC" 'avy-goto-char-timer))
 
+;;NOTE: This package contains bugs for motions like w, but I don't use those.
+;;https://github.com/PythonNut/evil-easymotion/pull/71
+;;https://github.com/erikvader/evil-easymotion/tree/eriks
 (use-package evil-easymotion
+  :ensure t
   :config
+  (evilem-make-motion eriks/evilem-next-line-last-non-blank #'eriks/evil-next-line-last-non-blank)
+  (evilem-make-motion eriks/evilem-previous-line-last-non-blank #'eriks/evil-previous-line-last-non-blank)
   (eriks/leader-def 'motion
     "j" #'evilem-motion-next-line
     "k" #'evilem-motion-previous-line
+    "J" #'eriks/evilem-next-line-last-non-blank
+    "K" #'eriks/evilem-previous-line-last-non-blank
     "+" #'evilem-motion-next-line-first-non-blank
     "-" #'evilem-motion-previous-line-first-non-blank))
