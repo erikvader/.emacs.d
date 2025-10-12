5cd6dffd221eb93282b94d0ec85675c9e4f088cc
Erik Rimskog <erik.rimskog@gmail.com>
2025-10-12 22:28:45 +0200

    git stuff

--- init.d/40-tools.el
+++ init.d/40-tools.el
@@ -7,22 +7,15 @@
   (global-eldoc-mode)
   :diminish eldoc-mode)
 
-;;TODO: how does this interact with tramp?
-(use-package add-node-modules-path
-  :ensure t
-  :config
-  (add-to-list 'safe-local-eval-forms '(add-node-modules-path)))
-
 (use-package apheleia
   :ensure t
-  :diminish "Aph"
-  :general-config
-  ("C-c C-f" 'apheleia-format-buffer))
+  :diminish "Aph")
 
 (use-package projectile
   :ensure t
   :diminish
   :custom
+  (projectile-find-dir-includes-top-level t)
   (projectile-current-project-on-switch 'keep)
   (projectile-ignored-project-function (cl-defun eriks/projectile-ignore-project (truename)
                                          "Ignore the sources of rust packages."
--- init.d/50-git.el
+++ init.d/50-git.el
@@ -1,7 +1,11 @@
 (use-package magit
   :ensure t
   :custom
-  (magit-diff-refine-hunk 'all)
+  (magit-define-global-key-bindings nil)
+  (magit-diff-refine-hunk nil)
+  (evil-collection-magit-use-$-for-end-of-line nil)
+  (evil-collection-magit-use-0-for-beginning-of-line nil)
+  (evil-collection-magit-use-z-for-folds t)
   :config
   (defun eriks/magit-refresh-with-all-untracked-files ()
     "Shows all untracked files in the current buffer, and do not stop at
@@ -14,24 +18,52 @@ untracked directories."
   (eriks/leader-def 'normal
     :infix "g"
     "s" 'magit-status
-    "b" 'magit-blame
-    "f" 'magit-find-file)
+    "f" 'magit-file-dispatch
+    "g" 'magit-dispatch)
 
-  (add-to-list 'evil-buffer-regexps
-               '("^COMMIT_EDITMSG$" . insert))
-  :gfhook ('magit-blame-mode-hook #'evil-emacs-state)
+  (eriks/leader-def 'normal 'magit-status-mode-map
+    "u" 'eriks/magit-refresh-with-all-untracked-files)
+
+  (evil-collection-magit-setup)
+  (evil-set-initial-state 'git-commit-mode 'insert)
   :general-config
+  ('(magit-revision-mode-map magit-status-mode-map)
+   ;; NOTE: let my leader through
+   "SPC" nil)
   ('magit-status-mode-map
-   :prefix "C-c"
-   "a" 'eriks/magit-refresh-with-all-untracked-files))
+   ;; NOTE: match the binding in `evil-collection-diff-mode-setup'
+   "g*" 'diff-refine-hunk)
+  ('normal
+   'magit-mode-map
+   ;; NOTE: evil-collection shadows these motion state bindings on purpose, it seems, they
+   ;; at least aren't there. So this re-adds all of those z bindings from evil-maps.el.
+   "z^" 'evil-scroll-top-line-to-bottom
+   "z+" 'evil-scroll-bottom-line-to-top
+   "zt" 'evil-scroll-line-to-top
+   "z RET" 'evil-scroll-line-to-top-first-non-blank
+   "zz" 'evil-scroll-line-to-center
+   "z." 'evil-scroll-line-to-center-first-non-blank
+   "zb" 'evil-scroll-line-to-bottom
+   "z-" 'evil-scroll-line-to-bottom-first-non-blank
+   "zl" 'evil-scroll-column-right
+   "z <right>" 'evil-scroll-column-right
+   "zh" 'evil-scroll-column-left
+   "z <left>" 'evil-scroll-column-left
+   "ze" 'evil-scroll-end-column
+   "zs" 'evil-scroll-start-column
+   "zH" 'evil-scroll-left
+   "z S-<right>" 'evil-scroll-left
+   "zL" 'evil-scroll-right
+   "z S-<right>" 'evil-scroll-right))
 
 (use-package git-timemachine
   :ensure t
   :config
-  (add-to-list 'evil-buffer-regexps
-               '("^timemachine:" . emacs))
+  (add-to-list 'evil-buffer-regexps '("^timemachine:" . normal))
+  (evil-collection-git-timemachine-setup)
   (eriks/leader-def 'normal
-    "gt" 'git-timemachine))
+    :infix "g"
+    "t" 'git-timemachine))
 
 (use-package what-the-commit
   :ensure t
@@ -56,8 +88,10 @@ untracked directories."
     "j" 'diff-hl-next-hunk
     "k" 'diff-hl-previous-hunk
     "o" 'diff-hl-show-hunk)
+  ;; NOTE: this mode forcefully binds a map to `diff-hl-command-prefix', so this is a
+  ;; workaround to disable that binding.
+  (general-define-key :keymaps 'diff-hl-mode-map diff-hl-command-prefix nil)
   :custom
-  ;; TODO: disable the map somehow `diff-hl-command-prefix'
   (diff-hl-side 'right)
   (diff-hl-show-hunk-inline-popup-smart-lines nil)
   (diff-hl-show-hunk-inline-popup-hide-hunk t))
@@ -71,3 +105,7 @@ untracked directories."
   (eriks/leader-def 'normal
     :infix "g"
     "m" 'git-messenger:popup-message))
+
+(use-package vc
+  :custom
+  (vc-follow-symlinks nil))
