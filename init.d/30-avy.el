6582ae2f5c037ea66676de2c85a97cfd8ed02b42
Erik Rimskog <erik.rimskog@gmail.com>
2025-10-22 21:33:24 +0200

    fix bug in my own evilem next line

--- init.d/30-avy.el
+++ init.d/30-avy.el
@@ -34,17 +34,18 @@
   ;; NOTE: these are copies of the normal ones, but they respect `track-eol' and
   ;; `evil-track-eol' by allowing the `temporary-goal-column' to be
   ;; `most-positive-fixnum'.
+  (defun eriks/evilem-change-line-goal-column ()
+    (if (and (numberp temporary-goal-column)
+             (= temporary-goal-column most-positive-fixnum))
+        most-positive-fixnum
+      (current-column)))
   (evilem-make-motion evilem-motion-next-line2 #'next-line
                       :pre-hook (setq evil-this-type 'line)
-                      :bind ((temporary-goal-column (if (= temporary-goal-column most-positive-fixnum)
-                                                        most-positive-fixnum
-                                                      (current-column)))
+                      :bind ((temporary-goal-column (eriks/evilem-change-line-goal-column))
                              (line-move-visual nil)))
   (evilem-make-motion evilem-motion-previous-line2 #'previous-line
                       :pre-hook (setq evil-this-type 'line)
-                      :bind ((temporary-goal-column (if (= temporary-goal-column most-positive-fixnum)
-                                                        most-positive-fixnum
-                                                      (current-column)))
+                      :bind ((temporary-goal-column (eriks/evilem-change-line-goal-column))
                              (line-move-visual nil)))
   (eriks/leader-def 'motion
     "j" #'evilem-motion-next-line2
