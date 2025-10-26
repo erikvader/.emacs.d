aec2c2d874c57a602441e21f4efcd4465beb5f49
Erik Rimskog <erik.rimskog@gmail.com>
2025-10-26 22:34:35 +0100

    reincrease the dtrt verbosity

--- init.d/90-dtrt.el
+++ init.d/90-dtrt.el
@@ -1,9 +1,10 @@
-;; NOTE: Put this last to make sure it doesn't unnecessarily guess the indentation of buffers
+;; NOTE: Put this last to make sure it doesn't unnecessarily guess the indentation of
+;; buffers during initialization.
 (use-package dtrt-indent
   :ensure t
-  :diminish
+  :diminish ""
   :custom
-  (dtrt-indent-verbosity 0)
+  (dtrt-indent-verbosity 1)
   ;; NOTE: the definition of `dtrt-indent-global-mode' tries to specify that it should
   ;; only be enabled in prog-mode and text-mode, but there is an extra t there, which
   ;; makes it enabled everywhere. This is for some reason breaking python-eldoc.
