5dbb404bd3136251d669eab866851bef3e889e78
Erik Rimskog <erik.rimskog@gmail.com>
2026-05-11 21:00:57 +0200

    darkened diff colors again...

--- init.d/50-theme.el
+++ init.d/50-theme.el
@@ -158,9 +158,9 @@ the default number back to the usual 2."
                           (ovwr 'diff-refine-added :underline dracula-green)
                           (ovwr 'diff-refine-removed :underline dracula-red)
                           (ovwr 'diff-refine-changed :underline dracula-yellow)
-                          (ovwr 'diff-added :foreground dracula-fg :background (color-darken-name dracula-green 80) :extend t)
-                          (ovwr 'diff-removed :foreground dracula-fg :background (color-darken-name dracula-red 80) :extend t)
-                          (ovwr 'diff-changed :foreground dracula-fg :background (color-darken-name dracula-yellow 80) :extend t)
+                          (ovwr 'diff-added :foreground dracula-fg :background (color-darken-name dracula-green 85) :extend t)
+                          (ovwr 'diff-removed :foreground dracula-fg :background (color-darken-name dracula-red 85) :extend t)
+                          (ovwr 'diff-changed :foreground dracula-fg :background (color-darken-name dracula-yellow 85) :extend t)
                           ;; Smerge
                           (ovwr 'smerge-upper :inherit 'magit-diff-our)
                           (ovwr 'smerge-base :inherit 'magit-diff-base)
