a89330df45958ae2d4481952c6d9a9b2551ce466
Erik Rimskog <erik.rimskog@gmail.com>
2026-05-15 18:10:46 +0200

    remove unused functions

--- init.d/03-my-functions.el
+++ init.d/03-my-functions.el
@@ -1,36 +1,3 @@
-(defun eriks/display-buffer-same-window (buffer alist)
-  "A rule thingy for `display-buffer'. Run
-`display-buffer-same-window' but also mark the opened window to
-have spawned the frame by itself. This makes it play nice with
-`frames-only-mode' because spawned frames will close themselves
-when `quit-window' is run.
-
-See `same-buffer'."
-  (let ((w (display-buffer-same-window buffer alist)))
-    (when (windowp w)
-      (display-buffer-record-window 'frame w buffer)
-      (set-window-prev-buffers w nil))
-    w))
-
-;; TODO: is this still needed since `frames-only-mode' is gone?
-(defmacro same-buffer (&rest body)
-  "Run BODY with `display-buffer-overriding-action' set to
-  `eriks/display-same-window'. In other words, this will run BODY and
-  open all new buffers in the same window. The purpose of this macro
-  is to have emacsclient play nicely with `frames-only-mode'.
-
-Example:
-  emacsclient -n -c -e '(same-buffer (find-file \"asd\"))'
-This will open a new frame visiting \"asd\". When the buffer is
-`quit-window':ed the spawned frame will close itself.
-
-One could also use `display-buffer-pop-up-frame' instead, but that
-will not allow for prompts like the too-large-file warning to work and
-it will not allow for `man' to know how wide it's window is."
-  `(let ((display-buffer-overriding-action '(eriks/display-buffer-same-window
-                                             (inhibit-same-window . nil))))
-     ,@body))
-
 ;; TODO: remove?
 (defmacro eriks/unset-key (keymap key)
   "Completely removes KEY from KEYMAP. Just binding it to nil still
