d1cb14a0f53336f5fd017e666b90b6b254de4a0f
Erik Rimskog <erik.rimskog@gmail.com>
2026-05-25 23:09:52 +0200

    second look at lsp-mode, but i still don't want to use it

--- init.d/70-lsp.el
+++ init.d/70-lsp.el
@@ -1,11 +1,8 @@
-;; TODO: remove this file?
-
-;; TODO: breadcrumbs?
 (use-package lsp-mode
   :disabled
   :ensure t
   :custom
-  (lsp-prefer-flymake nil)
+  (lsp-enable-on-type-formatting nil)
   (lsp-enable-snippet nil)
   (lsp-enable-indentation nil)
   (lsp-enable-symbol-highlighting nil)
@@ -13,11 +10,16 @@
   (lsp-headerline-breadcrumb-enable nil)
   (lsp-lens-enable nil)
   (lsp-completion-provider :none)
+  (lsp-format-buffer-on-save t)
+  (lsp-keymap-prefix nil)
+  (lsp-eldoc-render-all nil)
+  (lsp-inlay-hint-enable t)
+  (lsp-modeline-code-action-icons-enable nil) ;; TODO: did this remove the icon?
   :config
   (eriks/leader-def
     'normal
     'lsp-mode-map
-    "l" 'lsp-command-map)
+    "l" lsp-command-map)
 
   (defun lsp-toggle-highlighting ()
     "Toggles `lsp-enable-symbol-highlighting'.
@@ -25,6 +27,7 @@
 source: https://github.com/emacs-lsp/lsp-mode/issues/515#issuecomment-564665576"
     (interactive)
     (setq lsp-enable-symbol-highlighting (not lsp-enable-symbol-highlighting))
+    ;; TODO: find a better way to make lsp mode realise the changed setting
     (cond
      ((and lsp-enable-symbol-highlighting  (lsp--capability "documentHighlightProvider"))
       (add-hook 'lsp-on-idle-hook #'lsp--document-highlight nil t)
@@ -35,6 +38,7 @@ source: https://github.com/emacs-lsp/lsp-mode/issues/515#issuecomment-564665576"
       (lsp--info "Highlighting disabled."))
      (t (user-error "Current server does not support highlights?"))))
 
+  ;; TODO: native solution?
   (defun eriks/lsp-if-already-started ()
     "Runs `lsp' only if it would connect to an already running
 server on some workspace."
@@ -51,60 +55,13 @@ server on some workspace."
                                ;; this wanted?? What is a multiroot workspace?
                                (lsp--find-workspace session client project-root))
                              clients))
-          (lsp)))))
+          (lsp))))))
 
-  ;; TODO: replaced by `lsp-format-buffer-on-save'?
-  (defun eriks/lsp-install-format-on-save-hooks-toggle (&optional arg)
-    "Toggle whether `lsp-format-buffer' should be run before
-save. Turn on if ARG is positive, toggle if 0 and turn off if
-negative."
-    (interactive "P")
-    (let ((arg (if (null arg)
-                   0
-                 (prefix-numeric-value arg))))
-      (cond ((and (<= arg 0)
-                  (local-variable-p 'before-save-hook)
-                  (memq #'lsp-format-buffer before-save-hook))
-             (remove-hook 'before-save-hook #'lsp-format-buffer t)
-             (when (interactive-p)
-               (message "no longer formatting on save")))
-            ((and (>= arg 0)
-                  (null (memq #'lsp-format-buffer before-save-hook)))
-             ;;TODO: does order with `lsp--before-save' matter?
-             (add-hook 'before-save-hook #'lsp-format-buffer nil t)
-             (when (interactive-p)
-               (message "formatting on save enabled")))
-            ((interactive-p)
-             (message "formatting on save already on/off")))))
-
-  (defvar-local eriks/lsp-format-on-save nil
-    "Whether `lsp-format-buffer' should run on save")
-  (put 'eriks/lsp-format-on-save 'safe-local-variable #'booleanp)
-  (defun eriks/lsp-format-on-save-hook ()
-    (if (bound-and-true-p eriks/lsp-format-on-save)
-        (eriks/lsp-install-format-on-save-hooks-toggle 1)
-      ;; If this hook is run before local variables are set, make sure it is run after.
-      ;; https://stackoverflow.com/a/5148435
-      (add-hook 'hack-local-variables-hook #'eriks/lsp-format-on-save-hook nil t)))
-  :gfhook
-  ('lsp-mode-hook 'eriks/lsp-format-on-save-hook))
-
-;; TODO: (define-key lsp-ui-mode-map [remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
-;;TODO: (define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references)
 (use-package lsp-ui
-  :disabled
   :ensure t
-  :ghook 'lsp-mode
   :custom
-  (lsp-ui-doc-enable nil) ;; TODO: hur få fram en skrollbar doc?
-  (lsp-ui-flycheck-enable t)
-  (lsp-ui-sideline-delay 1))
-;; TODO: funkar imenu?
-
-(use-package lsp-ui-sideline
-  :disabled
-  :gfhook
-  ('flycheck-mode-hook 'lsp-ui-sideline-mode))
+  (lsp-ui-sideline-enable nil)
+  (lsp-ui-doc-enable nil))
 
 (use-package lsp-pyls
   :disabled
@@ -114,14 +71,17 @@ negative."
   :gfhook
   ('python-mode-hook 'eriks/lsp-if-already-started))
 
+;; TODO: open external docs, bind it
 (use-package lsp-rust
   :disabled
   :custom
   (lsp-rust-clippy-preference "on")
   (lsp-rust-analyzer-closing-brace-hints nil)
-  (lsp-rust-analyzer-call-info-full nil)
   :gfhook
-  ('rust-mode-hook 'eriks/lsp-if-already-started))
+  ('rust-mode-hook 'eriks/lsp-if-already-started)
+  :gfhook
+  ('lsp-mode-map
+   [remap pp-macroexpand-last-sexp] #'lsp-rust-analyzer-expand-macro))
 
 (use-package ccls
   :disabled
@@ -133,4 +93,7 @@ negative."
   :disabled
   :ensure t)
 
-;; TODO: https://github.com/emacs-lsp/lsp-ivy??
+;; TODO: bind `lsp-ivy-workspace-symbol' to something
+(use-package lsp-ivy
+  :disabled
+  :ensure t)
